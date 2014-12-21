b6b571089affb54fca8d4f401b4d7bd53da3d0bd
Sunny Ngan <sunny.ngan.gh@gmail.com>
2014-12-21 13:46:30 -0500

    Moving function to scope

--- roman.js
+++ roman.js
@@ -7,50 +7,49 @@
             ['X', 'L'],
             ['C', 'D'],
             ['M']
-        ],
-        repeat = function (char, times) {
-            var result = '';
-            while(times > 0) {
-                result = result + char;
-                times -= 1;
-            }
-            return result;
-        },
-        getRomanString = function(num, order) {
-            var ref = romanString[order],
-                mod5 = num % 5,
-                rString = '';
-
-            if (num === 9) {
-                rString = ref[0] + romanString[order + 1][0];
-            } else if (mod5 === 4) {
-                rString = ref.join('');
-            } else {
-                if (num > 4) {
-                    rString = ref[1];
-                }
-                rString = rString + repeat(ref[0], mod5);
-            }
-
-            return rString;
-        };
-
+        ];
 
     Roman.getRomanFromNum = function(number) {
         if (!number || number < 1 || number > 4999) {
             throw new Error("Number must be between 1 and 4999");
         }
 
-        var result = '',
+        var repeat = function (char, times) {
+                var result = '';
+                while(times > 0) {
+                    result = result + char;
+                    times -= 1;
+                }
+                return result;
+            },
+            getRomanString = function(num, order) {
+                var ref = romanString[order],
+                mod5 = num % 5,
+                rString = '';
+
+                if (num === 9) {
+                    rString = ref[0] + romanString[order + 1][0];
+                } else if (mod5 === 4) {
+                    rString = ref.join('');
+                } else {
+                    if (num > 4) {
+                        rString = ref[1];
+                    }
+                    rString = rString + repeat(ref[0], mod5);
+                }
+
+                return rString;
+            },
+            result = '',
             order = 0,
             current;
-        while (number % 10 !== number){
+
+        while (number / 10 > 0){
             current = number % 10;
             result = getRomanString(current, order) + result;
             number = Math.floor(number / 10);
             order = order + 1;
         }
-        result = getRomanString(number, order) + result;
 
         return result;
     };
