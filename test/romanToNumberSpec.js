cd7be0a8d057c431fdb7045e9e3b873c2ed99949
Sunny Ngan <sunny.ngan.gh@gmail.com>
2014-12-21 14:57:13 -0500

    More unhappy case

--- test/romanToNumberSpec.js
+++ test/romanToNumberSpec.js
@@ -3,14 +3,14 @@ var Roman = require('../Roman').Roman;
 describe("Roman numeral to number", function() {
     'use strict';
 
-    it("should convert 'MMXIV' correctly", function() {
-        var TWO_THOUSAND_FOURTEEN = Roman.getNumFromRoman('MMXIV');
-        expect(TWO_THOUSAND_FOURTEEN).toBe(2014);
+    it("should convert 'MMXV' correctly", function() {
+        var TWO_THOUSAND_FIFTEEN = Roman.getNumFromRoman('MMXV');
+        expect(TWO_THOUSAND_FIFTEEN).toBe(2015);
     });
 
-    it("should convert 'MCXVI' correctly", function() {
-        var ONE_ONE_ONE_SIX = Roman.getNumFromRoman('MCXVI');
-        expect(ONE_ONE_ONE_SIX).toBe(1116);
+    it("should convert 'CMXCIX' correctly", function() {
+        var NINE_NINE_NINE = Roman.getNumFromRoman('CMXCIX');
+        expect(NINE_NINE_NINE).toBe(999);
     });
 
     it("should convert 'MCCCXXXVII' correctly", function() {
@@ -21,5 +21,11 @@ describe("Roman numeral to number", function() {
     it("should convert 'CCCXIV' correctly", function() {
         var PIE = Roman.getNumFromRoman('CCCXIV');
         expect(PIE).toBe(314);
+    });
+
+    it("should throw error for converting invalid string", function() {
+        expect(Roman.getNumFromRoman.bind(null, null)).toThrow(new Error("Invalid roman numeral string"));
+        expect(Roman.getNumFromRoman.bind(null, 'ICUP')).toThrow(new Error("Invalid roman numeral string"));
+        expect(Roman.getNumFromRoman.bind(null, 'MIXCD')).toThrow(new Error("Invalid roman numeral string"));
     });
 });
