ba4be59a3ec2370faa95d128c120f519b32736fc
Sunny Ngan <sunny.ngan.gh@gmail.com>
2014-12-20 23:39:41 -0500

    changed my mind, putting file in root

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'),
     jshint = require('gulp-jshint');
 
 gulp.task('lint', function() {
-    return gulp.src(['src/roman.js', 'test/*.js'])
+    return gulp.src(['roman.js', 'test/*.js'])
         .pipe(jshint('.jshintrc'))
         .pipe(jshint.reporter('jshint-stylish'))
         .pipe(jshint.reporter('fail'));
