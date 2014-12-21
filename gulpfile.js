72db282d63e7784c00d3b8be0502e382f8b7b2b2
Sunny Ngan <sunny.ngan.gh@gmail.com>
2014-12-20 23:35:03 -0500

    jshint, make lint a default gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint');
 
-gulp.task('default', function() {
-
+gulp.task('lint', function() {
+    return gulp.src(['src/roman.js', 'test/*.js'])
+        .pipe(jshint('.jshintrc'))
+        .pipe(jshint.reporter('jshint-stylish'))
+        .pipe(jshint.reporter('fail'));
 });
+
+gulp.task('default', ['lint']);
